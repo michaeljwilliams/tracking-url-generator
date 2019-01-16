8c69a2b4dd5b5716adaa69ca4895a35cdddaedc2
michael <mwilliams@dcsbusiness.com>
2019-01-16 11:55:04 -0800

    Use switch statement instead.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tracking-url-generator
-Generate tracking URLs for packages shipped with common carriers.
+Generate tracking URLs for packages shipped with common carriers, given carrier name and tracking number.
+
+Example:
+```js
+const tug = require("tracking-url-generator");
+
+const trackingURL = tug("Canada Post", "000000000000000");
+console.log(trackingURL);
+"http//www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=000000000000000"
+
+// Unsupported carrier
+const anotherTrackingURL = tug("Royal Mail", "000000000000000");
+console.log(anotherTrackingURL);
+false // Carrier isn't supported in this package (yet)
+```
+
--- index.js
+++ index.js
@@ -7,49 +7,36 @@
 // Internal modules
 
 const tug = function(carrier, trackingNumber="XXXXXXXXXXXXXXX") {
-    const carriers = {
-        USPS: {
-            name: "USPS",
-            url: `https://tools.usps.com/go/TrackConfirmAction?tLabels=${trackingNumber}`
-        },
-        UPS: {
-            name: "UPS",
-            url: `http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=${trackingNumber}`
-        },
-        FedEx: {
-            name: "FedEx",
-            url: `http://www.fedex.com/Tracking?action=track&tracknumbers=${trackingNumber}`
-        },
-        LaserShip: {
-            name: "LaserShip",
-            url: `http://www.lasership.com/track/LS${trackingNumber}`
-        },
-        DHLUS: {
-            name: "DHL US",
-            url: `http://www.dhl.com/en/express/tracking.html?AWB=${trackingNumber}`
-        },
-        DHLGlobal: {
-            name: "DHL Global",
-            url: `http://webtrack.dhlglobalmail.com/?trackingnumber=${trackingNumber}`
-        },
-        OnTrac: {
-            name: "OnTrac",
-            url: `http://www.ontrac.com/trackingdetail.asp?tracking=${trackingNumber}`
-        },
-        ICCWorld: {
-            name: "ICC World",
-            url: `http://iccworld.com/track.asp?txtawbno=${trackingNumber}`
-        },
-        CanadaPost: {
-            name: "Canada Post",
-            url: `http://www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=${trackingNumber}`
-        }
-    };
+    switch(carrier) {
+        case "USPS":
+            return `https//tools.usps.com/go/TrackConfirmAction?tLabels=${trackingNumber}`;
 
-    if(carriers[carrier]) {
-        return carriers[carrier];
-    } else {
-        return false;
+        case "UPS":
+            return `http//wwwapps.ups.com/WebTracking/track?track=yes&trackNums=${trackingNumber}`;
+
+        case "FedEx":
+            return `http//www.fedex.com/Tracking?action=track&tracknumbers=${trackingNumber}`;
+
+        case "LaserShip":
+            return `http//www.lasership.com/track/LS${trackingNumber}`;
+
+        case "DHL US":
+            return `http//www.dhl.com/en/express/tracking.html?AWB=${trackingNumber}`;
+
+        case "DHL Global":
+            return `http//webtrack.dhlglobalmail.com/?trackingnumber=${trackingNumber}`;
+
+        case "OnTrac":
+            return `http//www.ontrac.com/trackingdetail.asp?tracking=${trackingNumber}`;
+
+        case "ICC World":
+            return `http//iccworld.com/track.asp?txtawbno=${trackingNumber}`;
+
+        case "Canada Post":
+            return `http//www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=${trackingNumber}`;
+
+        default:
+            return false;
     }
 };
 
