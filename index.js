069b0dc23ed62d147b7da6c5ada80e77cef81354
michael <mwilliams@dcsbusiness.com>
2019-01-20 14:31:36 -0800

    Fix URLs and use https.

--- index.js
+++ index.js
@@ -9,31 +9,28 @@
 const tug = function(carrier, trackingNumber="XXXXXXXXXXXXXXX") {
     switch(carrier) {
         case "USPS":
-            return `https//tools.usps.com/go/TrackConfirmAction?tLabels=${trackingNumber}`;
+            return `https://tools.usps.com/go/TrackConfirmAction?tLabels=${trackingNumber}`;
 
         case "UPS":
-            return `http//wwwapps.ups.com/WebTracking/track?track=yes&trackNums=${trackingNumber}`;
+            return `https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=${trackingNumber}`;
 
         case "FedEx":
-            return `http//www.fedex.com/Tracking?action=track&tracknumbers=${trackingNumber}`;
+            return `https://www.fedex.com/Tracking?action=track&tracknumbers=${trackingNumber}`;
 
         case "LaserShip":
-            return `http//www.lasership.com/track/LS${trackingNumber}`;
+            return `https://www.lasership.com/track/LS${trackingNumber}`;
 
         case "DHL US":
-            return `http//www.dhl.com/en/express/tracking.html?AWB=${trackingNumber}`;
+            return `http://www.dhl.com/en/express/tracking.html?AWB=${trackingNumber}`;
 
         case "DHL Global":
-            return `http//webtrack.dhlglobalmail.com/?trackingnumber=${trackingNumber}`;
+            return `https://webtrack.dhlglobalmail.com/?trackingnumber=${trackingNumber}`;
 
         case "OnTrac":
-            return `http//www.ontrac.com/trackingdetail.asp?tracking=${trackingNumber}`;
-
-        case "ICC World":
-            return `http//iccworld.com/track.asp?txtawbno=${trackingNumber}`;
+            return `https://www.ontrac.com/trackingdetail.asp?tracking=${trackingNumber}`;
 
         case "Canada Post":
-            return `http//www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=${trackingNumber}`;
+            return `https://www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=${trackingNumber}`;
 
         default:
             return false;
